ea71714c383c505c00617dc572317c96fcf451a7
LucasMarquesDePaula <marques.analista@gmail.com>
2017-03-04 18:14:50 -0300

    Configurado Hibernate

---
+++ src/main/java/br/ufpr/gepta/qrcdoor/model/Pessoa.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufpr.gepta.qrcdoor.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import static javax.swing.text.StyleConstants.Size;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Lucas
+ */
+@Entity
+@Table(name = "Pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idPessoa;
+
+    @Size(min = 0, max = 255)
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    /**
+     * @return the idPessoa
+     */
+    private int getIdPessoa() {
+        return idPessoa;
+    }
+
+    /**
+     * @param idPessoa the idPessoa to set
+     */
+    private void setIdPessoa(int idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    /**
+     * @return the nome
+     */
+    private String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + idPessoa;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof Pessoa)) {
+            return false;
+        }
+        Pessoa other = (Pessoa) obj;
+        if (this.idPessoa != other.idPessoa) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [idPessoa=" + idPessoa + ", nome=" + nome + "]";
+    }
+}
