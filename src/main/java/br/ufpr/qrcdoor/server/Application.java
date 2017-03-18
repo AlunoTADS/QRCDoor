ffd6f13285b601cf4b3570f3ecaab9b9f21eb990
LucasMarquesDePaula <marques.analista@gmail.com>
2017-03-18 20:54:08 -0300

    Teste na minha maquina OK

--- src/main/java/br/ufpr/qrcdoor/server/Application.java
+++ src/main/java/br/ufpr/qrcdoor/server/Application.java
@@ -2,7 +2,8 @@ package br.ufpr.qrcdoor.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
