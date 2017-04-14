d352ae178c15cd305f8ea2963d589940a59b4035
LucasMarquesDePaula <marques.analista@gmail.com>
2017-04-14 13:49:39 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	SistemaAutomovel/src/br/ufpr/view/Main.java

---
+++ src/main/java/br/ufpr/qrcdoor/repository/PessoaRepository.java
@@ -0,0 +1,16 @@
+package br.ufpr.qrcdoor.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import br.ufpr.qrcdoor.entity.Pessoa;
+
+@RepositoryRestResource(collectionResourceRel = "pessoa", path = "pessoa")
+public interface PessoaRepository extends PagingAndSortingRepository<Pessoa, Long> {
+	
+	List<Pessoa> findByNome(@Param("name") String name);
+
+}
