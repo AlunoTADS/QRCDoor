b486800f69f9e184f1f2252d0a3142bb2605eb9a
giovannibeltrame <gio.beltrame@gmail.com>
2017-04-14 19:06:04 -0300

    Entities created

--- src/main/java/br/ufpr/qrcdoor/entity/Chave.java
+++ src/main/java/br/ufpr/qrcdoor/entity/Chave.java
@@ -1,50 +1,154 @@
 package br.ufpr.qrcdoor.entity;
 
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
 
 /**
- *
- * @author Lucas
+ * The persistent class for the "Chave" database table.
+ * 
  */
 @Entity
+@Table(name="\"Chave\"")
+@NamedQuery(name="Chave.findAll", query="SELECT c FROM Chave c")
 public class Chave implements Serializable {
-    	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "idchave")
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="\"idChave\"")
 	private Long id;
-	
-	private String codigo;
 
-    /**
-     * @return the id
-     */
-    private Long getId() {
-        return id;
-    }
+	@Temporal(TemporalType.DATE)
+	@Column(name="\"dataFim\"")
+	private Date dataFim;
 
-    /**
-     * @param id the id to set
-     */
-    private void setId(Long id) {
-        this.id = id;
-    }
+	@Temporal(TemporalType.DATE)
+	@Column(name="\"dataInicio\"")
+	private Date dataInicio;
 
-    /**
-     * @return the codigo
-     */
-    private String getCodigo() {
-        return codigo;
-    }
+	private String descricao;
 
-    /**
-     * @param codigo the codigo to set
-     */
-    private void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
+	@Column(name="\"idPessoa\"")
+	private Long idPessoa;
+
+	private String situacao;
+
+	//bi-directional many-to-one association to Acesso
+	@OneToMany(mappedBy="chave")
+	private List<Acesso> acessos;
+
+	//bi-directional many-to-one association to Pessoa
+	@ManyToOne
+	@JoinColumns({
+		})
+	private Pessoa pessoa;
+
+	//bi-directional one-to-one association to QRCode
+	@OneToOne(mappedBy="chave")
+	private QRCode qrcode;
+
+	//bi-directional one-to-one association to Senha
+	@OneToOne(mappedBy="chave")
+	private Senha senha;
+
+	public Chave() {
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDataFim() {
+		return this.dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public Date getDataInicio() {
+		return this.dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getIdPessoa() {
+		return this.idPessoa;
+	}
+
+	public void setIdPessoa(Long idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getSituacao() {
+		return this.situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+	public List<Acesso> getAcessos() {
+		return this.acessos;
+	}
+
+	public void setAcessos(List<Acesso> acessos) {
+		this.acessos = acessos;
+	}
+
+	public Acesso addAcesso(Acesso acesso) {
+		getAcessos().add(acesso);
+		acesso.setChave(this);
+
+		return acesso;
+	}
+
+	public Acesso removeAcesso(Acesso acesso) {
+		getAcessos().remove(acesso);
+		acesso.setChave(null);
+
+		return acesso;
+	}
+
+	public Pessoa getPessoa() {
+		return this.pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public QRCode getQrcode() {
+		return this.qrcode;
+	}
+
+	public void setQrcode(QRCode qrcode) {
+		this.qrcode = qrcode;
+	}
+
+	public Senha getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(Senha senha) {
+		this.senha = senha;
+	}
+
 }
--- src/main/java/br/ufpr/qrcdoor/entity/PermissaoPessoa.java
+++ src/main/java/br/ufpr/qrcdoor/entity/PermissaoPessoa.java
@@ -1,14 +1,108 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufpr.qrcdoor.entity;
 
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
 /**
- *
- * @author Lucas
+ * The persistent class for the "PermissaoPessoa" database table.
+ * 
  */
-public class PermissaoPessoa {
-    
+@Entity
+@Table(name="\"PermissaoPessoa\"")
+@NamedQuery(name="PermissaoPessoa.findAll", query="SELECT p FROM PermissaoPessoa p")
+public class PermissaoPessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="\"idPermissaoPessoa\"")
+	private Long id;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="\"dataFim\"")
+	private Date dataFim;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="\"dataInicio\"")
+	private Date dataInicio;
+
+	@Column(name="\"idPermissao\"")
+	private Long idPermissao;
+
+	@Column(name="\"idPessoa\"")
+	private Long idPessoa;
+
+	//bi-directional many-to-one association to Permissao
+	@ManyToOne
+	@JoinColumns({
+		})
+	private Permissao permissao;
+
+	//bi-directional many-to-one association to Pessoa
+	@ManyToOne
+	@JoinColumns({
+		})
+	private Pessoa pessoa;
+
+	public PermissaoPessoa() {
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDataFim() {
+		return this.dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public Date getDataInicio() {
+		return this.dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Long getIdPermissao() {
+		return this.idPermissao;
+	}
+
+	public void setIdPermissao(Long idPermissao) {
+		this.idPermissao = idPermissao;
+	}
+
+	public Long getIdPessoa() {
+		return this.idPessoa;
+	}
+
+	public void setIdPessoa(Long idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public Permissao getPermissao() {
+		return this.permissao;
+	}
+
+	public void setPermissao(Permissao permissao) {
+		this.permissao = permissao;
+	}
+
+	public Pessoa getPessoa() {
+		return this.pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
 }
