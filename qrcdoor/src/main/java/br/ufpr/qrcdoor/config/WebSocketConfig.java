66985eb7dd460e82020b704f2a7802a4e5240cf8
LucasMarquesDePaula <marques.analista@gmail.com>
2017-09-13 00:04:42 -0300

    WebSocket without STOMP

--- qrcdoor/src/main/java/br/ufpr/qrcdoor/config/WebSocketConfig.java
+++ qrcdoor/src/main/java/br/ufpr/qrcdoor/config/WebSocketConfig.java
@@ -1,52 +1,34 @@
 package br.ufpr.qrcdoor.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
-import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 import br.ufpr.qrcdoor.interceptor.HttpSessionIdHandshakeInterceptor;
-import br.ufpr.qrcdoor.interceptor.WebSocketSessionCapturingHandlerDecorator;
+import br.ufpr.qrcdoor.ws.WebSocketMessageHandler;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-//		config.enableStompBrokerRelay("/queue", "/topic/");
-		config.enableSimpleBroker("/open");
-		config.setApplicationDestinationPrefixes("/app");
-	}
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+        .addHandler(messageHandler(), "/open")
+        .withSockJS()
+        .setInterceptors(httpSessionIdHandshakeInterceptor());
+    }
 
-
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws-door-manager").withSockJS().setInterceptors(httpSessionIdHandshakeInterceptor());
-	}
-
+    @Bean
+    public WebSocketHandler messageHandler() {
+        return new WebSocketMessageHandler();
+    }
+    
 	@Bean
 	public HttpSessionIdHandshakeInterceptor httpSessionIdHandshakeInterceptor() {
 		return new HttpSessionIdHandshakeInterceptor();
-	}
-
-	@SuppressWarnings("unused")
-	@Autowired
-	private WebSocketHandler subProtocolWebSocketHandler;
-
-	@Override
-	public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-		registration.addDecoratorFactory(new WebSocketHandlerDecoratorFactory() {
-			@Override
-			public WebSocketHandler decorate(WebSocketHandler webSocketHandler) {
-				return new WebSocketSessionCapturingHandlerDecorator(webSocketHandler);
-			}
-		});
 	}
 }
