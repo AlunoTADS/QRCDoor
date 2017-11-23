6e0340817e12b2ced6397c8d02a5a25e4b0769e0
Giovanni Beltrame <gio.beltrame@gmail.com>
2017-11-23 21:51:15 -0200

    Permissao extending GenericService

--- qrcdoor/src/main/java/br/ufpr/qrcdoor/service/PermissaoService.java
+++ qrcdoor/src/main/java/br/ufpr/qrcdoor/service/PermissaoService.java
@@ -1,38 +1,25 @@
 package br.ufpr.qrcdoor.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import br.ufpr.qrcdoor.entity.Permissao;
-import br.ufpr.qrcdoor.exception.ResourceNotFoundException;
 import br.ufpr.qrcdoor.repository.PermissaoRepository;
 
 @Service
-public class PermissaoService {
+public class PermissaoService extends GenericService<Permissao, Long> {
 	
 	@Autowired
 	PermissaoRepository permissaoRepository;
-	
-	public List<Permissao> findAll() throws Exception {
-		return this.permissaoRepository.findAll();
-	}
-	
-	public Permissao findOne(Long id) throws Exception {
-		Permissao permissao = this.permissaoRepository.findOne(id);
-		if (permissao == null) {
-			throw new ResourceNotFoundException();
-		}
-		return permissao;
-	}
 	
 	public Permissao save(Permissao permissao) throws Exception {
 		return this.permissaoRepository.saveAndFlush(permissao);
 	}
 	
-	public void delete(Long id) throws Exception {
-		this.permissaoRepository.delete(id);
+	@Override
+	public JpaRepository<Permissao, Long> getRepository() {
+		return this.permissaoRepository;
 	}
 
 }
